5659caf45acdfa5d61e7333cf4dae1aef1ed9987
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-08-11 23:25:46 +0530

    First Commit

--- src/javaprogramming1/practice/Practice1.java
+++ src/javaprogramming1/practice/Practice1.java
@@ -4,7 +4,7 @@ public class Practice1 {
     public static void main(String args[]) {
         //Data types
         //numerical data type
-        int byte_var = 10; //1byte
+        byte byte_var = 10; //1byte
         short short_var = 32000;//2byte
         int int_var = 8;//4byte
         long long_var = 420000000;//8byte
@@ -17,6 +17,30 @@ public class Practice1 {
         System.out.println("OR :- " + (byte_var | int_var)); // 2
         System.out.println("Left shift :- " + (byte_var >> 1)); // 5
         System.out.println("Right shift :- " + (byte_var << 1)); // 4
+
+        //assignment
+        byte_var += int_var;
+        System.out.println("Addtion assignment :- " + byte_var);
+        byte_var -= int_var;
+        System.out.println("Subtraction assignment :- " + byte_var);
+        byte_var *= int_var;
+        System.out.println("Mulitplication assignment :- " + byte_var);
+        byte_var /= int_var;
+        System.out.println("Division assignment :- " + byte_var);
+
+        //comparison
+        System.out.println("Grater than :- " + (byte_var > int_var));
+        System.out.println("less than :- " + (byte_var < int_var));
+        System.out.println("Greater than equal to :- " + (byte_var >= int_var));
+        System.out.println("Less than equal to :- " + (byte_var <= int_var));
+        System.out.println("Equal to :- " + (byte_var == int_var));
+        System.out.println("Not equal to :- " + (byte_var != int_var));
+
+        //logical operators
+        System.out.println("Logical AND :- " + ((byte_var > int_var) && (byte_var < int_var)));
+        System.out.println("Logical OR :- " + ((byte_var > int_var) || (byte_var < int_var)));
+        System.out.println("logical NOT :- " + !((byte_var > int_var || (byte_var < int_var))));
+
 
 
     }
