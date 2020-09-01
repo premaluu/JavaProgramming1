a90559a76b8a4a1898d6dc6d0a9b14ce71bbf560
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-09-01 12:02:27 +0530

    Basic Java

--- src/javaprogramming1/practice/Practice1.java
+++ src/javaprogramming1/practice/Practice1.java
@@ -1,5 +1,4 @@
 package javaprogramming1.practice;
-
 public class Practice1 {
     public static void main(String[] args) {
         //Data typesLq
@@ -15,8 +14,8 @@ public class Practice1 {
         //bitwise
         System.out.println("AND :- " + (byte_var & int_var)); // 8
         System.out.println("OR :- " + (byte_var | int_var)); // 2
-        System.out.println("Left shift :- " + (byte_var >> 1)); // 5
-        System.out.println("Right shift :- " + (byte_var << 1)); // 4
+        System.out.println("Left shift :- " + (byte_var << 1)); // 20 1010 -> 10100
+        System.out.println("Right shift :- " + (byte_var >> 1)); // 5 1010 -> 0101
 
         //assignment
         byte_var += int_var;
@@ -41,5 +40,66 @@ public class Practice1 {
         System.out.println("Logical OR :- " + ((byte_var > int_var) || (byte_var < int_var)));
         System.out.println("logical NOT :- " + !((byte_var > int_var || (byte_var < int_var))));
 
+        //Conditional Statement
+        if(byte_var > short_var) {
+            System.out.println("Byte is greater");
+        } else if(short_var > byte_var) {
+            System.out.println("Short is greater");
+        } else {
+            if(int_var > byte_var && int_var > short_var) {
+                System.out.println("Integer is greater than all");
+            } else {
+                System.out.println("Short is greater then all");
+            }
+        }
+        //Switch statement
+        int choice = 4;
+        switch(choice) {
+            case 1:
+                System.out.println("Byte var is selected ");
+                break;
+            case 2:
+                System.out.println("Short var is selected");
+                break;
+            case 3:
+                System.out.println("Int var is selected");
+                break;
+            case 4:
+                System.out.println("Long var is selected");
+                break;
+            default:
+                System.out.println("Enter the valid choice (Among 1 to 4)");
+                break;
+        }
+
+        //looping
+        int i = 0;
+        while(i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i < 5);
+
+        for(i = 0 ; i < 5 ; i++) {
+            System.out.println(i);
+        }
+        int arr[] = {10, 20,30, 40, 50, 60, 70, 80, 90, 100};
+        for(int ele : arr) {
+            System.out.println(ele);
+        }
+        outerLoop:
+        for(i = 0 ; i < 5 ; i++) {
+            for(int j = 0 ; j < 5 ; j++) {
+                if(i >= 1) {
+                    break outerLoop;
+                } else {
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
