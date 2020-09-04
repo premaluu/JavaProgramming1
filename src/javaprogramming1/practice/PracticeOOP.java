cef360d55db5ab88e837c2ceb74a23120bb3a80b
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-09-05 01:07:40 +0530

    OOP JAVA

--- src/javaprogramming1/practice/PracticeOOP.java
+++ src/javaprogramming1/practice/PracticeOOP.java
@@ -3,34 +3,28 @@ package javaprogramming1.practice;
 //class
 class Bike {
 	int speed;
-	static String company = "Honda";
-
+	static String company;
 	//constructor
 	public Bike() {
-		this.speed = 50;
+		speed = 10;
 		company = "Honda";
 	}
-
 	public Bike(int speed) {
 		this.speed = speed;
 	}
 	public Bike(Bike bike) {
 		this.speed = bike.speed;
 	}
-
 	//methods
 	public int getSpeed() {
 		return this.speed;
 	}
-
-	public String getCompany() {
-		return company;
-	}
-
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
-
+	public String getCompany() {
+		return Bike.company;
+	}
 	//static methods
 	public static void setCompany(String company1) {
 		company = company1;
@@ -38,10 +32,15 @@ class Bike {
 }
 public class PracticeOOP {
 	public static void main(String args[]) {
-		System.out.println("Speed :- "+new Bike(50).getSpeed());
-		System.out.println("Speed :- "+new Bike(60).getSpeed());
+		//  Bike bike = new Bike();
+		System.out.println("Speed :- "+new Bike(50).getSpeed()); //annonymous object
 		Bike bike = new Bike();
+		bike.setSpeed(60);
+		System.out.println("Speed :- "+bike.getSpeed());
+		System.out.println("Company :- "+bike.getCompany());
+		System.out.println("Speed :- "+new Bike(bike).getSpeed());
 		Bike.setCompany("Suzuki");
-		System.out.println("Company :- "+Bike.company);
+		System.out.println("Company :- "+bike.getCompany());
+		System.out.println("Speed :- "+bike.getSpeed());
 	}
 }
