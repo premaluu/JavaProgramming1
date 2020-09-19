12745825487858ca62b3bf66c95feafdb0061d04
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-09-20 01:23:32 +0530

    OOP commit

--- src/javaprogramming1/practice/PracticeOOP.java
+++ src/javaprogramming1/practice/PracticeOOP.java
@@ -1,57 +1,90 @@
 package javaprogramming1.practice;
 
 //class
-class Bike {
-	//instance variable
-	int speed;
-	static String company = "Honda";
+
+class Engine {
+	int hp;
+	int CC;
 	
-	//static block
-	static {
-		company = "Honda";
-	}
-	//constructor
-	public Bike() {
-		this.speed = 50;
+	public Engine() {
+		hp = 5000;
+		CC = 450;
 	}
 	
-	public Bike(int speed) {
-		this.speed = speed;
-	}
-	
-	public Bike(Bike bike) {
-		this.speed = bike.speed;
+	public Engine(int hp, int CC) {
+		this.hp = hp;
+		this.CC = CC;
 	}
 	
 	//methods
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public void setCC(int CC) {
+		this.CC = CC;
+	}
+	
+	public int getHp() {
+		return this.hp;
+	}
+	
+	public int getCC() {
+		return this.CC;
+	}
+}
+class Bike {
+	// instance variable
+	int speed;
+	static String company = "Honda";
+
+	// static block
+	static {
+		company = "Honda";
+	}
+
+	// constructor
+	public Bike() {
+		this.speed = 50;
+	}
+
+	public Bike(int speed) {
+		this.speed = speed;
+	}
+
+	public Bike(Bike bike) {
+		this.speed = bike.speed;
+	}
+
+	// methods
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
-	
+
 	public int getSpeed() {
 		return this.speed;
 	}
-	
+
 	public String getCompany() {
 		return company;
 	}
-	
+
 	public Bike getObject() {
 		return this;
 	}
 
-	//static methods
+	// static methods
 	public static void setCompany(String company1) {
 		company = company1;
 	}
 }
 
 class StandardBike extends Bike {
-	//instance variable
+	// instance variable
 	private int fuelCapacity;
 	private int hp;
 
-	//constructor
+	// constructor
 	public StandardBike() {
 		super();
 		this.fuelCapacity = 20;
@@ -75,145 +108,162 @@ class StandardBike extends Bike {
 		this.fuelCapacity = standardBike.fuelCapacity;
 		this.hp = standardBike.hp;
 	}
-	
-	//methods
+
+	// methods
 	public void setFuelCapacity(int fuelCapacity) {
 		this.fuelCapacity = fuelCapacity;
 	}
-	
+
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
-	
+
 	public int getFuelCapacity() {
 		return this.fuelCapacity;
 	}
-	
+
 	public int getHp() {
 		return this.hp;
 	}
- }
+}
 
 class SportsBike extends Bike {
-	//instance
+	// instance
 	int maxSpeed;
 	int manufacturingYear;
-	
-	//constructor
+
+	// constructor
 	public SportsBike() {
 		super(100);
 		company = "Honda";
 	}
-	
+
 	public SportsBike(int maxSpeed) {
 		super(100);
 		this.maxSpeed = maxSpeed;
 	}
-	
+
 	public SportsBike(int maxSpeed, int speed) {
 		super(speed);
 		this.maxSpeed = maxSpeed;
 	}
-	
+
 	public SportsBike(int maxSpeed, int manufacturingYear, int speed) {
 		super(speed);
 		this.maxSpeed = maxSpeed;
 		this.manufacturingYear = manufacturingYear;
 	}
-	
+
 	public SportsBike(SportsBike sportsBike) {
 		super(sportsBike);
 		this.maxSpeed = sportsBike.maxSpeed;
 		this.manufacturingYear = sportsBike.manufacturingYear;
 	}
-	
-	//methods
+
+	// methods
 	public void setMaxSpeed(int maxSpeed) {
 		this.maxSpeed = maxSpeed;
 	}
-	
+
 	public void setManufacturingYear(int manufacturingYear) {
 		this.manufacturingYear = manufacturingYear;
 	}
-	
+
 	public int getMaxSpeed() {
 		return this.maxSpeed;
 	}
-	
+
 	public int getManufacturingYear() {
 		return this.manufacturingYear;
 	}
 }
+
 class Hayabusa extends SportsBike {
 	String color;
-	
-	//constructor
+	private Engine engine; //Aggression
+	// constructor
 	public Hayabusa() {
 		speed = 150;
 		company = "Suzuki";
 	}
-	
+
 	public Hayabusa(int speed) {
 		super(speed);
 	}
-	
-	public Hayabusa(int speed, int MaxSpeed) {
-		super(speed, MaxSpeed);
+
+	public Hayabusa(int speed, int maxSpeed) {
+		super(speed, maxSpeed);
 	}
-	
-	public Hayabusa(int speed, int MaxSpeed, int manufacturingYear) {
-		super(speed, MaxSpeed, manufacturingYear);
+
+	public Hayabusa(int speed, int maxSpeed, int manufacturingYear) {
+		super(speed, maxSpeed, manufacturingYear);
 	}
-	
-	public Hayabusa(int speed, int MaxSpeed, int manufacturingYear, String color) {
-		super(speed, MaxSpeed, manufacturingYear);
+
+	public Hayabusa(int speed, int maxSpeed, int manufacturingYear, String color) {
+		super(speed, maxSpeed, manufacturingYear);
 		this.color = color;
 	}
 	
-	//methods
+	public Hayabusa(int speed, int maxSpeed, int manufacturingYear, Engine engine) {
+		super(speed, maxSpeed, manufacturingYear);
+		this.engine = engine;
+	}
+
+	public Hayabusa(int speed, int maxSpeed, int manufacturingYear, String color,Engine engine) {
+		super(speed, maxSpeed, manufacturingYear);
+		this.color = color;
+		this.engine = engine;
+	}
+	// methods
 	public void setColor(String color) {
 		this.color = color;
 	}
-	
+
 	public String getColor() {
 		return this.color;
 	}
+	
+	public String getEngineDetails() {
+		return "Hp :- "+this.engine.hp+"\nCC :- "+this.engine.CC;
+	}
 }
+
 public class PracticeOOP {
 	public static void main(String args[]) {
 		Bike bike = new Bike();
-		System.out.println("Speed :- "+bike.getSpeed());
-		System.out.println("Company :- "+bike.getCompany());
-		System.out.println("Speed :- "+new Bike(60).getSpeed());
+		System.out.println("Speed :- " + bike.getSpeed());
+		System.out.println("Company :- " + bike.getCompany());
+		System.out.println("Speed :- " + new Bike(60).getSpeed());
 		Bike.setCompany("Suzuki");
-		System.out.println("Speed :- "+bike.getSpeed());
-		System.out.println("Company :- "+bike.getCompany());
+		System.out.println("Speed :- " + bike.getSpeed());
+		System.out.println("Company :- " + bike.getCompany());
 		Bike bike1 = bike.getObject();
-		System.out.println("Speed :- "+bike1.getSpeed());
-		System.out.println("Company :- "+bike1.getCompany());
-		
+		System.out.println("Speed :- " + bike1.getSpeed());
+		System.out.println("Company :- " + bike1.getCompany());
+
 		StandardBike standardBike = new StandardBike(20, 1500);
 		StandardBike standardBike1 = new StandardBike(standardBike);
-		System.out.println("Speed :- "+standardBike1.getSpeed());
-		System.out.println("Company :- "+standardBike1.getCompany());
-		
+		System.out.println("Speed :- " + standardBike1.getSpeed());
+		System.out.println("Company :- " + standardBike1.getCompany());
+
 		SportsBike sportsBike = new SportsBike(450, 2010, 150);
-		System.out.println("Speed :- "+sportsBike.getSpeed());
-		System.out.println("Hight Speed :- "+sportsBike.getMaxSpeed());
-		System.out.println("Manufacturing year :- "+sportsBike.getManufacturingYear());
-		System.out.println("Company :- "+sportsBike.getCompany());
-		
+		System.out.println("Speed :- " + sportsBike.getSpeed());
+		System.out.println("Hight Speed :- " + sportsBike.getMaxSpeed());
+		System.out.println("Manufacturing year :- " + sportsBike.getManufacturingYear());
+		System.out.println("Company :- " + sportsBike.getCompany());
+
 		SportsBike newSportsBike = new SportsBike(sportsBike);
-		System.out.println("Speed :- "+sportsBike.getSpeed());
-		System.out.println("Hight Speed :- "+sportsBike.getMaxSpeed());
-		System.out.println("Manufacturing year :- "+sportsBike.getManufacturingYear());
-		System.out.println("Company :- "+sportsBike.getCompany());
+		System.out.println("Speed :- " + sportsBike.getSpeed());
+		System.out.println("Hight Speed :- " + sportsBike.getMaxSpeed());
+		System.out.println("Manufacturing year :- " + sportsBike.getManufacturingYear());
+		System.out.println("Company :- " + sportsBike.getCompany());
 		System.out.println();
-		Hayabusa hayabusa = new Hayabusa(150, 450, 2010, "White");
-		System.out.println("Speed :- "+hayabusa.getSpeed());
-		System.out.println("High speed :- "+hayabusa.getMaxSpeed());
-		System.out.println("Manufacturing year :- "+hayabusa.getManufacturingYear());
-		System.out.println("Color :- "+hayabusa.getColor());
-		System.out.println("Company :- "+hayabusa.getCompany());
+		Hayabusa hayabusa = new Hayabusa(150, 450, 2010, "White", new Engine(5000, 700));
+		System.out.println("Speed :- " + hayabusa.getSpeed());
+		System.out.println("High speed :- " + hayabusa.getMaxSpeed());
+		System.out.println("Manufacturing year :- " + hayabusa.getManufacturingYear());
+		System.out.println("Color :- " + hayabusa.getColor());
+		System.out.println("Company :- " + hayabusa.getCompany());
+		System.out.println(hayabusa.getEngineDetails());
 	}
 }
